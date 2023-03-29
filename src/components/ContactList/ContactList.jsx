32659ad863b53d8a870f3287e8346aba806efa11
KlapijcukDima <98531117+KlapijcukDima@users.noreply.github.com>
2023-03-29 15:54:49 +0300

    done

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,19 +1,21 @@
 import PropTypes from 'prop-types';
 import s from './ContactList.module.css';
 
-const ContactList = ({ contacts, handleDeleteContacts }) => {
+const ContactList = ({ filter, contacts, handleDeleteContacts }) => {
   return (
     <ul className={s.list}>
-      {contacts.map(el => (
-        <li key={el.id} className={s.item}>
-          <p className={s.text}>
-            {el.name}: <span className={s.num}>{el.number}</span>
-          </p>
-          <button type="button" className={s.btn} onClick={() => handleDeleteContacts(el.id)}>
-            x
-          </button>
-        </li>
-      ))}
+      {contacts
+        .filter(el => el.name.toLowerCase().includes(filter.toLocaleLowerCase()))
+        .map(el => (
+          <li key={el.id} className={s.item}>
+            <p className={s.text}>
+              {el.name}: <span className={s.num}>{el.number}</span>
+            </p>
+            <button type="button" className={s.btn} onClick={() => handleDeleteContacts(el.id)}>
+              x
+            </button>
+          </li>
+        ))}
     </ul>
   );
 };
@@ -21,6 +23,7 @@ const ContactList = ({ contacts, handleDeleteContacts }) => {
 export default ContactList;
 
 ContactList.propTypes = {
+  filter: PropTypes.string.isRequired,
   contacts: PropTypes.array.isRequired,
   handleDeleteContacts: PropTypes.func.isRequired,
 };
